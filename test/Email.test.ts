4e1f44afd9786a14d09ea06b65f90e7db81bfd87
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-06-06 19:06:15 -0300

    test: email test

---
+++ test/Email.test.ts
@@ -0,0 +1,10 @@
+import Email from "../src/domain/vo/Email"
+
+test("Deve criar um email válido", function() {
+    const email = new Email("joe.doe@gmail.com")
+    expect(email.getValue()).toBe("joe.doe@gmail.com")
+})
+
+test("Não deve criar um email válido", function() {
+    expect(() => new Email("joe.doe")) 
+})
