56044db356e326caa9ce7019d1af9fba36497659
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-06-06 19:06:20 -0300

    test: period test

---
+++ test/Period.test.ts
@@ -0,0 +1,13 @@
+import Period from "../src/domain/vo/Period"
+
+test("Deve criar um período válido", function() {
+    const period = new Period("2024-06-06T10:00:00", "2024-06-07T10:00:00")
+    expect(period.getStart()).toEqual(new Date("2024-06-06T10:00:00"))
+    expect(period.getEnd()).toEqual(new Date("2024-06-07T10:00:00"))
+    expect(period.getDurationInDays()).toBe(1)
+    expect(period.getDurationInHours()).toBe(24)
+})
+
+test("Não deve criar um período válido", function() {
+    expect(() => new Period("2024-06-08T10:00:00", "2024-06-07T10:00:00")).toThrow(new Error("Invalid period"))
+})
