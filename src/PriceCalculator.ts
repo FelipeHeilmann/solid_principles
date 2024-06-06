71abc789a80c17656a4a5d90464728e1ad63cd14
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-06-06 13:43:51 -0300

    feat: price calculator

---
+++ src/PriceCalculator.ts
@@ -0,0 +1,34 @@
+import Period from "./Period";
+
+export default abstract class PriceCalculator {
+    calculate (period: Period, roomPrice: number): { duration: number, price: number } {
+		const duration = this.calculateDuration(period);
+		const price = duration * roomPrice
+		return {
+			duration,
+			price
+		}
+	}
+
+	abstract calculateDuration (period: Period): number;
+}
+
+export class DayPriceCalculator extends PriceCalculator {
+    calculateDuration(period: Period): number {
+        return period.getDurationInDays()
+    }
+}
+
+export class HourPriceCalculator extends PriceCalculator {
+    calculateDuration(period: Period): number {
+        return period.getDurationInDays()
+    }
+}
+
+export class PriceCalculatorFactory {
+    static create (type: string) {
+		if (type === "day") return new DayPriceCalculator()
+		if (type === "hour") return new HourPriceCalculator()
+		throw new Error()
+	}
+}
