c82622e2aa01de34662c92c26d05ecb47c00d26d
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-06-08 13:27:51 -0300

    test: email in use

--- test/Signup.test.ts
+++ test/Signup.test.ts
@@ -16,3 +16,25 @@ test("Deve criar uma conta de usuário", async function() {
     expect(output.id).toBeDefined()
     await databaseConnection.close()
 })
+
+test("Não deve criar uma conta de usuário com email já em uso", async function() {
+    const databaseConnection = new PgPromiseAdapter()
+    const accounRepository = new AccountRepositoryDatabase(databaseConnection)
+    const signup = new Signup(accounRepository)
+    const email =  `john.doe${Math.random()}@gmail.com`
+    const input = {
+        email,
+        name: "John Doe",
+        document: "094.365.850-04",
+        password:"12345"
+    }
+    const input2 = {
+        email,
+        name: "John Doe",
+        document: "016.455.080-13",
+        password:"123456789"
+    }
+    await signup.execute(input)
+    expect(() => signup.execute(input2)).rejects.toThrow(new Error("Email already in use"))
+    await databaseConnection.close()
+})
