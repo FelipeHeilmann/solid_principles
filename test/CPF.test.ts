bedba7cccdc4315e2d0d1ad74139386dd9660ef9
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-06-07 23:33:59 -0300

    test: cpf test

---
+++ test/CPF.test.ts
@@ -0,0 +1,21 @@
+import CPF from "../src/domain/vo/CPF";
+
+test.each([
+	"97456321558",
+	"71428793860",
+	"87748248800",
+    "170.689.010-93",
+    "016.455.080-13"
+])("Deve testar um cpf válido: %s", function (cpf: any) {
+	expect(new CPF(cpf)).toBeDefined()
+});
+
+test.each([
+	undefined,
+	null,
+	"11111111111",
+	"123",
+	"1234566789123456789"
+])("Deve testar um cpf inválido: %s", function (cpf: any) {
+	expect(() => new CPF(cpf)).toThrow(new Error("Invalid CPF"))
+});
