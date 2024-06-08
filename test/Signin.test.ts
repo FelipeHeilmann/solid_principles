648e56c213314a4a5cfcba353423fd7c6082678a
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-06-08 13:27:46 -0300

    test: invalid credentials

--- test/Signin.test.ts
+++ test/Signin.test.ts
@@ -27,3 +27,46 @@ test("Deve fazer o login de um usuário", async function() {
     expect(outputSignin.token).toBeDefined()
     await databaseConnection.close()
 })
+
+test("Não deve fazer login para usuário com email inválido", async function() {
+    const databaseConnection = new PgPromiseAdapter()
+    const accountRepository = new AccountRepositoryDatabase(databaseConnection)
+    const inputSignup = {
+        email: `john.doe${Math.random()}@gmail.com`,
+        name: "John Doe",
+        document: "094.365.850-04",
+        password:"12345"
+    }
+    const signup = new Signup(accountRepository)
+    await signup.execute(inputSignup)
+    const inputSignin = {
+        email: "john.doe@gmail.com",
+        password: "12345"
+    }
+    const tokenService = new JwtTokenAdapter()
+    const signin = new Signin(accountRepository, tokenService)
+    expect(() => signin.execute(inputSignin)).rejects.toThrow(new Error("Invalid credentials"))
+    await databaseConnection.close()
+})
+
+test("Não deve fazer o login de um usuário com senha inválida", async function() {
+    const databaseConnection = new PgPromiseAdapter()
+    const accountRepository = new AccountRepositoryDatabase(databaseConnection)
+    const email = `john.doe${Math.random()}@gmail.com`
+    const inputSignup = {
+        email,
+        name: "John Doe",
+        document: "094.365.850-04",
+        password:"12345"
+    }
+    const signup = new Signup(accountRepository)
+    await signup.execute(inputSignup)
+    const inputSignin = {
+        email,
+        password: "123456"
+    }
+    const tokenService = new JwtTokenAdapter()
+    const signin = new Signin(accountRepository, tokenService)
+    expect(() => signin.execute(inputSignin)).rejects.toThrow(new Error("Invalid credentials"))
+    await databaseConnection.close()
+})
