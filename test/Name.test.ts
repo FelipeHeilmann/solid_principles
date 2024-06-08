c6b94bd3a8559a24387a1a9d98d224d9392c05ad
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-06-07 23:31:18 -0300

    feat: name test

---
+++ test/Name.test.ts
@@ -0,0 +1,10 @@
+import Name from "../src/domain/vo/Name"
+
+test("Deve criar um nome válido", function() {
+    const name = new Name("Jonas Furtado")
+    expect(name.getValue()).toBe("Jonas Furtado")
+})
+
+test("Não deve criar um nome válido", function() {
+    expect(() => new Name("Jonas")).toThrow(new Error("Invalid name"))
+})
